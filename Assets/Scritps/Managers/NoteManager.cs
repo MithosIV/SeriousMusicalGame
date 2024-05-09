270a80c4339cb31fb336f3232d61717935d613fd
MithosIV <101414251+MithosIV@users.noreply.github.com>
2024-05-09 02:11:10 -0600

    Delayed Play Sounds

--- Assets/Scritps/Managers/NoteManager.cs
+++ Assets/Scritps/Managers/NoteManager.cs
@@ -2,26 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class NoteManager : MonoBehaviour
+public class NoteManager : MonoBehaviour
+{
+    //[SerializeField] private PlayerController player;
+    AudioSource audioSource;
+    void Start()
     {
-        //[SerializeField] private PlayerController player;
-        AudioSource audioSource;
-        void Start()
-        {
 
-        }
-        private void FixedUpdate()
-        {
+    }
+    private void FixedUpdate()
+    {
 
-        }
-        void Update()
-        {
+    }
+    void Update()
+    {
 
-        }
-        public void PlayNoteAudio(GameObject notePlatform)
+    }
+    public void PlayNoteAudio(GameObject notePlatform)
+    {
+        audioSource = notePlatform.gameObject.GetComponent<AudioSource>();
+        audioSource.Play();
+        Debug.Log("Reproduciendo sonido de " + notePlatform.name);
+    }
+    public void PlayMelody(List<GameObject> melody)
+    {
+        for (int i = 0; i < melody.Count; i++)
         {
-            audioSource = notePlatform.gameObject.GetComponent<AudioSource>();
-            audioSource.Play();
-            Debug.Log("Reproduciendo sonido de " + notePlatform.name);
+            audioSource = melody[i].GetComponent<AudioSource>();
+            audioSource.PlayDelayed(audioSource.clip.length);
+            //yield return new WaitForSeconds(audioSource.clip.length);
         }
     }
+}
